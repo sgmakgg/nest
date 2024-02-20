98c9100b7aa344cf5be6f35f6b33408444f2df37
MSV <sgmakgg@gmail.com>
2024-02-20 17:55:13 +0300

    chapter 3 PSQL&TORM implemented with CRUD operations

--- iluvcoffee/src/coffees/coffees.module.ts
+++ iluvcoffee/src/coffees/coffees.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import {CoffeesController} from "./coffees.controller";
 import {CoffeesService} from "./coffees.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Coffee} from "./entities/cofees.entity";
 
 @Module({
+    imports: [TypeOrmModule.forFeature([Coffee])],
     controllers: [CoffeesController],
     providers: [CoffeesService]
 })
