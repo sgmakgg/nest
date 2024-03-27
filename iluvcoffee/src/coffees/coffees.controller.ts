5d6f4aecceff67b0bc55c065d68fcb1d33808c1a
MSV <sgmakgg@gmail.com>
2024-03-27 11:07:22 +0300

    custom pipes, middleware, custom decorators

--- iluvcoffee/src/coffees/coffees.controller.ts
+++ iluvcoffee/src/coffees/coffees.controller.ts
@@ -3,7 +3,7 @@ import {
   Controller,
   Delete,
   Get, Inject,
-  Param,
+  Param, ParseIntPipe,
   Patch,
   Post,
   Query, SetMetadata, UsePipes, ValidationPipe
@@ -14,6 +14,7 @@ import {UpdateCoffeeDto} from "./dto/update-coffee.dto/update-coffee.dto";
 import {PaginationQueryDto} from "../common/dto/pagination-query.dto";
 import {REQUEST} from "@nestjs/core";
 import {Public} from "../common/decorators/public.decorator";
+import {Protocol} from "../common/decorators/protocol.decorator";
 
 // @UsePipes(ValidationPipe) // controller scoped pipe
 @Controller('coffees')
@@ -27,13 +28,16 @@ export class CoffeesController {
   @Public()
   @UsePipes(ValidationPipe) //method scoped pipe
   @Get()
-  findAll(@Query() paginationQuery: PaginationQueryDto) {
+  async findAll(@Protocol('https') protocol: string, @Query() paginationQuery: PaginationQueryDto) {
+    console.log(protocol);
+    //await new Promise(resolve => setTimeout(resolve, 5000));
     return this.coffeesService.findAll(paginationQuery);
   }
 
   @Public()
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id', ParseIntPipe) id: string) {
+    console.log(id)
     return this.coffeesService.findOne(id);
   }
 
